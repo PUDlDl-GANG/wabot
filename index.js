e4e2058d01fbf410282a5efa47213c79e6e93d22
RmsBOT7 <75308262+RmsBOT7@users.noreply.github.com>
2020-12-19 00:36:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -267,7 +267,7 @@ conn.sendMessage(id, 'Sama sama, semoga harimu menyenangkan :)' ,MessageType.tex
 
 if (text.includes('.nulis')){
   var teks = text.replace(/.nulis /, '')
-    axios.get('https://bangandre.herokuapp.com/nulis?teks='+teks)
+    axios.get('https://tobz-api.herokuapp.com/api/nulis?text=${teks}')
     .then((res) => {
       imageToBase64(res.data.result)
         .then(
@@ -280,7 +280,7 @@ if (text.includes('.nulis')){
 }
 if (text.includes('.nulis')){
   var teks = text.replace(/.nulis /, '')
-    axios.get('https://bangandre.herokuapp.com/nulis?teks='+teks)
+    axios.get('https://tobz-api.herokuapp.com/api/nulis?text='+teks)
     .then((res) => {
       imageToBase64(res.data.result)
         .then(
